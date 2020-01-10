46f32859fad10d422c2639f01ed2b8ae5e923d19
Michelle Scott <scottmm374@gmail.com>
2020-01-09 22:17:29 -0500

    MVP and Stretch done

--- recipes/recipes-router.js
+++ recipes/recipes-router.js
@@ -12,6 +12,16 @@ router.get("/", async (req, res, next) => {
   }
 });
 
+router.get("/:id/shoppingList", async (req, res, next) => {
+  try {
+    const recipes = await Recipes.getShoppingList(req.params.id);
+    res.json(recipes);
+  } catch (err) {
+    console.log(err, "err");
+    next(err);
+  }
+});
+
 router.get("/:id/instructions", async (req, res, next) => {
   try {
     const instructions = await Recipes.getInstructions(req.params.id);
